6b02a4c7cce8dce717e2adb3d3c8d4a21a0cf2fd
thakurshilpa <thakurshilpa556@gmail.com>
2019-04-15 10:13:24 +0530

    getinfo.js

--- helloworld.js
+++ helloworld.js
@@ -1,11 +1,20 @@
 var express = require('express');  
 var app = express();  
 var port =  8000;  
+
   
-app.get("/nearbyservice",function(request,response)  
+app.get('/',function(request,response)  
 {  
     response.json({"Message":"hello Node js"});    //key value
-});  
+}); 
+
+//require('./commands.js')(app);
+const data = require('/list.json')
+
+app.get('/list:id', function (req, res) {
+  res.header("Content-Type",'application/json');
+  res.send(JSON.stringify(data));
+})
   
 app.listen(port, function () {  
      
