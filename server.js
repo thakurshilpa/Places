cc1274be429f1eb195c56c53359b61c47f0a4c65
thakurshilpa <thakurshilpa556@gmail.com>
2019-05-22 10:29:42 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict'
+var express = require('express');
+var app = express();
+let port = 8001;
+app.listen(port);
+
+console.log("Server running on port: " + port);		
+
+
+let places = require('./getplaces.js')
+// Register endpoint handlers here
+app.get('/getplaces',function(req, res) {
+    var result = places.getplaces(req.query.lat, req.query.lng,req.query.radius);
+	res.contentType('application/json');
+	res.end(JSON.stringify(result));
+});
+var request = require('request');
+var start = new Date();
+request.get({
+  url : 'getplaces.js',
+  time : true
+},function(err, response){
+	 console.log('Request time in ms', new Date() - start);
+   
+   
+})
+
+
+
+
