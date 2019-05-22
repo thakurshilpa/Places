a20d43471b87024a0d14962743bba19e0e1afacf
Chandan Thakur <chandan.thakur@nutanix.com>
2019-05-23 00:18:20 +0530

    Minor changes

--- getplaces.js
+++ getplaces.js
@@ -4,59 +4,38 @@ const fs = require('fs');
 //http://localhost:8001/getplaces?lat=1.30&lng=1.20
 
 // We should add restaurants to this file places_database.json
-let rawPlacesData = fs.readFileSync('Places1_database.json');
+let rawPlacesData = fs.readFileSync('allPlaces_database.json');
 let Places = JSON.parse(rawPlacesData);
 
 let getplaces = function(lat, lng,radius) {
+    var ts_begin = new Date();
     var result = [];
-    var d=[];
-
-    //Object.keys(req.query).length    one option
-    if(radius==' ')
-    {
-    var radiusa = 1.27; //KM
-
-    }
-    else
-    {
-        var radiusa=radius;
-    }
-   
-	for (var i=0;i<Places.length;i++) {
+    radius = radius || 5; // KM
+    console.log("Total Places: " + Places.length);
+	for (var i=0;i < Places.length; i++) {
 		let distance = getDistance(lat,lng, Places[i].geometry.location.lat, Places[i].geometry.location.lng);
-		console.log("Distance from start(" + lat + "," + lng + ") to end(" + Places[i].geometry.location.lat + "," + Places[i].geometry.location.lng + ") is " + distance + " KM");
-		if(distance <radiusa) { 
-
+		if(distance < radius) { 
+           console.log("Distance from start(" + lat + "," + lng + ") to end(" + Places[i].geometry.location.lat + "," + Places[i].geometry.location.lng + ") is " + distance + " KM");
 	 	   result.push(Places[i]);
 		}
-
-   
-	}
+    }
+    
     result.sort(function(a,b)
     {
-        for (var i=0;i<Places.length-1;i++)
-        {
-            let d1=getDistance(lat,lng, Places[i].geometry.location.lat, Places[i].geometry.location.lng);
-            let d2=getDistance(lat,lng, Places[i+1].geometry.location.lat, Places[i+1].geometry.location.lng);
-          return d1-d2;
-        }
-
+        let d1=getDistance(lat,lng, a.geometry.location.lat, a.geometry.location.lng);
+        let d2=getDistance(lat,lng, b.geometry.location.lat, b.geometry.location.lng);
+        return d1-d2;
     });
-console.log(result);
 
+    var ts_end = new Date();
+    var time_taken = ts_end - ts_begin;
+    console.log("Time taken: " + time_taken + " ms");
 	return result; 
 };
-
-if(typeof(Number.prototype.toRad) === "undefined") {
-    Number.prototype.toRad = function () {
-        return this * Math.PI / 180;
-    }
-}
 
 let toRad = function(value) {
 	 return value * Math.PI / 180;
 }
-
 
 // start and end are objects with latitude and longitude
 //decimals (default 2) is number of decimals in the output
@@ -76,7 +55,7 @@ let getDistance = function(lat1, lng1, lat2, lng2) {
     var dLon = toRad(lon2 - lon1);
     var lat1 = toRad(lat1);
     var lat2 = toRad(lat2);
-// c is the angular distance in radians, and a is the square of half the chord length between the points.//
+    // c is the angular distance in radians, and a is the square of half the chord length between the points.//
     var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
             Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
@@ -88,5 +67,4 @@ let getDistance = function(lat1, lng1, lat2, lng2) {
 module.exports = {
     getplaces: getplaces
 };
-
 
