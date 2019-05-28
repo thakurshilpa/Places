b7a88e82d61404fb424214953246262dd7d124e6
shilpa <thakurshilpa556@gmail.cpm>
2019-05-28 19:14:44 +0530

    not complete

--- quadtree.js
+++ quadtree.js
@@ -4,9 +4,48 @@ const fs = require('fs');
 
 let rawPlacesData = fs.readFileSync('allPlaces_database.json');
 let Places = JSON.parse(rawPlacesData);
- //let tl=[[]],tr=[[]],bl=[[]],br=[[]];  //store data in arrays 
-   let n=128;
+create(Places);
+
+function Node(Places,topLeft,topRight,bottomLeft,bottomRight) {
+     this.Places = Places;
+     this.topLeft =null;
+     this.topRight = null;
+
+     this.bottomLeft = null;
+     this.bottomRight = null;
+ };
+  
+
+  fuction create(Places){  //store data in arrays 
+  
    var tl=[];
-   tl[0]=[];
-   tl[0][0]=1;
-   console.log(tl[0][0])
+  var tr=[];
+  var bl=[];
+  var br=[];
+  if(l<500) return;
+  l=Places.length;
+    for(i=0;i<l/4;i++)
+  {
+  	tl[i].push(Places[i]);
+  	
+  }
+  for(i=l/4;i<(2*l)/4;i++)
+  {
+  	tr[i].push(Places[i]);
+  	
+  }
+  for(i=(2*l)/4;i<(3*l)/4;i++)
+  {
+  	t[i].push(Places[i]);
+  	
+  }
+  for(i=(3*l)/4;i<n;i++)
+  {
+  	tl[i].push(laces[i]);
+  	
+  }
+
+
+
+};
+
